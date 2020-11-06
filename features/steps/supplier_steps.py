24e7b0a5d6c1d4f9235184a93c28e6870fed12a9
Luolingwei <564258080@qq.com>
2020-11-06 13:33:50 -0500

    create skeleton for BDD

---
+++ features/steps/supplier_steps.py
@@ -0,0 +1,21 @@
+"""
+Supplier Steps
+
+Steps file for suppliers.feature
+
+"""
+
+from os import getenv
+import logging
+import json
+import requests
+from behave import *
+from compare import expect, ensure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions
+
+WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
+ID_PREFIX = 'supplier_'
+
---
+++ features/suppliers.feature
@@ -0,0 +1,4 @@
+Feature: The supplier service back-end
+    As a Supplier Manager
+    I need a RESTful catalog service
+    So that I can keep track of all my suppliers

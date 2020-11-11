b7110e1c22c9c595188fa3a4f5567ec28884c748
Yucong Liu <liuyucong6666@126.com>
2020-11-10 21:56:13 -0500

    Added Scenario update a supplier

--- features/steps/supplier_steps.py
+++ features/steps/supplier_steps.py
@@ -185,3 +185,14 @@ def step_impl(context, text_string, element_name):
         )
     )
     expect(found).to_be(True)
+
+@when('I change "{element_name}" to "{text_string}"')
+def step_impl(context, element_name, text_string):
+    element_id = ID_PREFIX + element_name.lower()
+    # element = context.driver.find_element_by_id(element_id)
+    element = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+    element.clear()
+    element.send_keys(text_string)
+    
